86eb63e5c1a27f6c27be9d5493c67f47c9690ad0
jainkrish <jainkrish.work@gmail.com>
2025-02-06 10:37:27 +0530

    Added user management feature

--- databse.sql
+++
@@ -1,9 +0,0 @@
--- Database Schema
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    dob DATE NOT NULL
-);
